3baaf934da5da532a96d0199e5306fc17e0df87e
Hanthebot <56969399+Hanthebot@users.noreply.github.com>
2024-08-25 02:28:23 +0800

    manual reverting

---
+++ src/data.js
@@ -0,0 +1,10 @@
+var data = {
+    "infoprocessing": {
+        "title": "Information Processor (24Sum)",
+        "path": "C:/Users/ruf77/OneDrive - HKUST Connect/Books/infoprocessing/book"
+    },
+    "zkp_mooc": {
+        "title": "Zero Knowledge Proofs MOOC (24F)",
+        "path": "C:/Users/ruf77/OneDrive - HKUST Connect/Books/zkp_mooc/book"
+    }
+};
